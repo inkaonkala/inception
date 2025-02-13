461cabaa44cb3dd0548e1931d6b73e8a3d7ac963
Inka Niska <70053619+inkaonkala@users.noreply.github.com>
2025-02-13 09:57:38 +0200

    update
    Now everything should be working

--- inception/srcs/requirements/wordpress/tools/setup.sh
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -2,35 +2,35 @@
 
 echo "Waiting for Maria <3 ..."
 while ! mysqladmin ping -h mariadb --silent; do
-	sleep 2
-	echo "WAITING"
+        sleep 2
+        echo "WAITING"
 done
 sleep 10
 
 echo "Maria is ready for WP"
 
-#install WP-CLI
+# Install WP-CLI
 if ! command -v wp &> /dev/null; then
-#if [ ! -f "/usr/local/bin/wp" ]; then
-	echo "Istalling WP_CLI ..."
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
+        echo "Installing WP-CLI ..."
+        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+        chmod +x wp-cli.phar
+        mv wp-cli.phar /usr/local/bin/wp
 fi
 
-#install phar
+# Install Phar
 if ! php -m | grep -q Phar; then
-   echo : "ERROR: Missing Phar"
+   echo "ERROR: Missing Phar"
    exit 1
 fi
 
+# Install WordPress
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-   echo "Installind WordPress .."
+   echo "Installing WordPress .."
    wp core download --path="/var/www/html" --allow-root
    wp config create --path="/var/www/html" --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --allow-root
-   wp core install  --path="/var/www/html" --url="https://$DOMAIN_NAME" --title="The Awesome WP Site, Yjay!" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="iniska@student.hive.fi" --allow-root
+   wp core install --path="/var/www/html" --url="https://$DOMAIN_NAME" --title="The Awesome WP Site" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$ADMIN_EMAIL" --allow-root
 
-   chown -R www-data:www-data /var/www/html
+   chown -R nobody:nogroup /var/www/html
    chmod -R 755 /var/www/html
 else
   echo "WP already installed"
@@ -38,8 +38,8 @@ fi
 
 echo "WP setup done!"
 
-#make the port not restart wrongly!
+# Ensure PHP-FPM listens on the correct port
 sed -i 's|listen = 127.0.0.1:9000|listen = 0.0.0.0:9000|' /etc/php81/php-fpm.d/www.conf
 
-
 exec php-fpm81 -F
+
